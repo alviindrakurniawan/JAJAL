3a52626a25a34976b372ee7a966a4bedf97d8c7b
Alvin Indra Kurniawan <83466710+alviindrakurniawan@users.noreply.github.com>
2021-11-24 07:33:48 +0700

    Update UserList.js

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,26 +1,40 @@
 import React, { useState,useEffect } from 'react'
 import axios from 'axios'
 import User from './User'
+import {ListGroup,ListGroupItem,Button} from 'reactstrap'
+
 
 export default function UserList() {
 	const [data, setData] = useState([]);
 	useEffect(async() => {
-		const hasil=await
 		axios.get("http://localhost:8080/mahasiswa")
 		.then((result)=>{setData(result.data)}).catch(error=>(console.log(error.message)))
 	
-		console.log(hasil)
 	})
+	
+
 
 	return (
-		<div>
-			
-			{data.map(el => {
-			<User
-			nama={el.nama}
-			nim={el.nim}
-			
-			/> })}
-		</div>
+		<ListGroup className="mt-2">
+			{data.map(el => (
+			<ListGroupItem key={el._id}>
+				<div>
+					{el.Nama}
+				</div>
+				<div>
+					{el.NIM}	
+				</div>
+				<div className="ml-auto"> 
+				<Button onClick={
+					()=>{
+						axios.delete("http://localhost:8080/mahasiswa/"+el._id)
+						.then(res=>console.log(res.data))
+					}
+
+				}> delete</Button>
+				</div>
+			</ListGroupItem>
+			))}
+		</ListGroup>
 	)
 }
